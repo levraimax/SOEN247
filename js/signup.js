0077031d3421ecb05e9e070d255a9fb12f5f5ad6
levraimax <levraimaximecharest@gmail.com>
2025-10-14 11:53:30 -0400

    Store profile data in session storage

--- js/signup.js
+++ js/signup.js
@@ -6,9 +6,21 @@ function every(arr, func) {
 }
 
 function signup() {
-    const form = document.querySelectorAll("div input:not([type='submit'])");
+    const form = document.querySelectorAll("div input:not([type='button'])");
     const passwords = document.querySelectorAll("div input[type='password']");
+
+
     if (every(form, (entry) => { return entry.value != "" }) && passwords[0].value == passwords[1].value) {
+        sessionStorage.setItem("user", profileJSON());  // Use a session cookie instead later
         window.location.href = '../html/loged_in.html'
     }
+}
+
+function profileJSON() {
+    const form = document.querySelectorAll("div input:not([type='button']):not([type='password'])");
+    let res = {};
+    form.forEach((entry) => {
+        res[entry.name] = entry.value;
+    });
+    return JSON.stringify(res);
 }
